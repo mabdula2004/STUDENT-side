99c14a3454db790c6f0a8b9d45b1ecbda916b192
mabdula2004 <muhammadabdula7874747@gmail.com>
2024-08-15 10:13:03 -0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'screens/register_student_page.dart';
+import 'screens/login_page.dart';
+import 'screens/student_home_screen.dart';
+import 'screens/task_list_screen.dart';
+import 'screens/student_performance_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,11 +17,18 @@ class StudentApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Student App',
+      title: 'Student Management App',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: TaskListScreen(),
+      initialRoute: FirebaseAuth.instance.currentUser == null ? '/login' : '/studentHome',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/registerStudent': (context) => RegisterStudentPage(),
+        '/studentHome': (context) => StudentHomeScreen(),
+        '/tasks': (context) => TaskListScreen(),
+        '/performance': (context) => StudentPerformanceScreen(studentId: ''),
+      },
     );
   }
 }
